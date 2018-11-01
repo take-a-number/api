f4915f48b0af292810dbab3a8f45e9657513fd45
Sameer <11097096+sameer@users.noreply.github.com>
2018-11-01 12:23:41 -0500

    Initialize Django Project

---
+++ take_a_number_api/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for take_a_number_api project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'take_a_number_api.settings')
+
+application = get_wsgi_application()
