73131ec1bb52fd56e1fbf9867ba24e297f080e3f
Sameer <11097096+sameer@users.noreply.github.com>
2018-11-05 21:16:07 -0600

    class -> course

--- navigation/urls.py
+++ navigation/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^class/(?P<name>[a-zA-Z0-9_.-]+)', views.course),
+    url(r'^course/(?P<name>[a-zA-Z0-9_.-]+)', views.course),
 ]
