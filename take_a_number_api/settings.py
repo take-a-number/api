5b9ac8c4fe9717e71bcbc269fdb60c4772488b3b
Sameer <11097096+sameer@users.noreply.github.com>
2018-11-05 20:09:00 -0600

    Use random bytes for secret key...

--- take_a_number_api/settings.py
+++ take_a_number_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dm=!x8^tftxp@i2=!yed8qyx@ek1vk)4+idtk=(+o0_q9cb!q-'
+SECRET_KEY = os.urandom(50)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
