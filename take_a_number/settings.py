a6169e7ff18e575427d9e9a6931a6545782e74d6
Joao Pan <panjoao@outlook.com>
2018-12-08 13:04:07 -0600

    active debugging

--- take_a_number/settings.py
+++ take_a_number/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.urandom(50)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['takeanumber-api.herokuapp.com', 'localhost']
 
