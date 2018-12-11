b2620459d1e0e60724835160e4cbffbc8086be91
emilymarkert <emily.markert@vanderbilt.edu>
2018-12-11 02:28:43 -0600

    adding first few integration tests

--- take_a_number/tests/test_integration.py
+++ take_a_number/tests/test_integration.py
@@ -4,6 +4,7 @@ from take_a_number.views import random_join_code
 from typing import Dict
 from take_a_number.models import Course
 import uuid
+import json
 
 
 class JoinCodeTest(TestCase):
@@ -17,8 +18,42 @@ class JoinCodeTest(TestCase):
 
 
 class ViewsTest(TestCase):
+    def test_course_create(self):
+        c = Client()
+
+        create_response = c.put('',
+                                b'"{\\"description\\":\\"Algorithms\\",\\"abbreviation\\":\\"CS 3250\\",\\"email\\":\\"e@m.vanderbilt.edu\\"}"')
+        self.assertEqual(create_response.status_code, 200)
+
+        # can't register the same class twice
+        create_response2 = c.put('',
+                                b'"{\\"description\\":\\"Algorithms\\",\\"abbreviation\\":\\"CS 3250\\",\\"email\\":\\"e@m.vanderbilt.edu\\"}"')
+        self.assertEqual(create_response2.status_code, 400)
+
     def test_identity(self):
-        return
+        c = Client()
+
+        create_response = c.put('', b'"{\\"description\\":\\"Algorithms\\",\\"abbreviation\\":\\"CS 3250\\",\\"email\\":\\"e@m.vanderbilt.edu\\"}"')
+        course_id = json.loads(create_response.content)
+
+        id_response = c.get('{}/office_hours/identity'.format(course_id))
+        course_dict = json.loads(id_response.content)
+        self.assertEqual("Vanderbilt University", course_dict["school"])
+        self.assertEqual("Algorithms", course_dict["description"])
+        self.assertEqual("CS 3250", course_dict["abbreviation"])
+        self.assertEqual("e.m@vanderbilt.edu", course_dict["email"])
+
+    def test_course_lookup(self):
+        c = Client()
+
+        create_response = c.put('',
+                                b'"{\\"description\\":\\"ISD\\",\\"abbreviation\\":\\"CS 3251\\",\\"email\\":\\"e@m.vanderbilt.edu\\"}"')
+        course_id = json.loads(create_response.content)
+
+        lookup_response = c.get('')
+        course_dict = json.loads(lookup_response.content)
+        # full_course_dict = {c_id: val.__dict__ for c_id, val in course_dict[0].items()}
+        self.assertEqual(course_dict[0]['id'], course_id)
 
     def test_students(self):
         return
@@ -28,6 +63,7 @@ class ViewsTest(TestCase):
 
     def test_office_hours(self):
         return
+
 
 
 
