a31e1380a67ff4db29d4353d50d0f04548aeffe0
Sameer <11097096+sameer@users.noreply.github.com>
2018-12-11 19:10:05 -0600

    Add django heroku to settings

--- take_a_number/settings.py
+++ take_a_number/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +134,6 @@ SESSION_COOKIE_SECURE = True
 #CSRF_COOKIE_HTTPONLY = False
 SESSION_COOKIE_HTTPONLY = False
 SESSION_COOKIE_SAMESITE = None
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
